aeb082be39790c1413580587a7e0b305b59cd90b
Abel Kalum <chumbabel@gmail.com>
2020-07-25 14:45:58 +0000

    modify users controller and views/signup.erb; create login.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,16 @@ class UsersController < ApplicationController
   get '/signup' do
     erb :signup
   end
+  
+  post '/signup' do
+    redirect '/login'
+  end
+  
+  get '/login' do
+    erb :login
+  end
+  
+  post '/login' do
+    redirect "users/#{@user.id}"
+  end
 end
