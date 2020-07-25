a7f23dbd80ecdfefe09e0d749d50213d1e1e87e7
Abel Kalum <chumbabel@gmail.com>
2020-07-25 17:58:34 +0000

    modify application controller, users controller, views/login.erb, views/signup.erb; create views/users/show.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,39 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
   end
 
   get "/" do
+    redirect_if_logged_in
     erb :welcome
+  end
+
+  helpers do
+
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+
+    def authorized_to_edit?(diary_entry)
+      diary_entry.user == current_user
+    end
+
+    def redirect_if_not_logged_in
+      redirect '/'
+    end
+
+    def redirect_if_logged_in
+      if logged_in?
+        redirect "/users/#{current_user.id}"
+      end
+    end
+
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,47 @@
 class UsersController < ApplicationController
- 
+
   get '/signup' do
+    redirect_if_logged_in
     erb :signup
   end
-  
-  post '/signup' do
-    redirect '/login'
+
+  post '/users' do
+    @user = User.new(params)
+    if @user.save
+      session[:user_id] = @user.id
+
+      redirect "/users/#{@user.id}"
+    else
+      redirect '/signup'
+    end
   end
   
   get '/login' do
+    redirect_if_logged_in
     erb :login
   end
-  
+
   post '/login' do
-    redirect "users/#{@user.id}"
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+
+      redirect "users/#{@user.id}"
+    else
+      redirect '/login'
+    end
   end
+
+  get '/users/:id' do
+    @user = User.find_by(id: params[:id])
+    redirect_if_not_logged_in
+
+    erb :'/users/show'
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/'
+  end
+
 end
